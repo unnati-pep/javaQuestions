d0ed6d774f8f76ed696efd1ccdf800d9faaf3beb
unnati-pep <Unnati.Sharma@pepsico.com>
2025-09-04 08:54:16 +0530

    Add files via upload

---
+++ assQ1.java
@@ -0,0 +1,22 @@
+public static class assQ1{
+    void ttheft(int[] arr,int k){
+        int total=0;
+        for(int j=0;j<k;j++){
+            int x=arr[0],ind=0;
+            for(int i=0;i<arr.length;i++){
+                if(x<arr[i] && arr[i]!=-1){
+                    x=arr[i];
+                    ind=i;
+                }
+            }
+            total+=x;
+            arr[ind]=-1;
+        }
+        System.out.println(total);
+    }
+    public static void main(String args[]){
+        int[] arr = {2,7,8,10,4,1,3};
+        int k=4;
+        ttheft(arr,k);
+    }
+}
